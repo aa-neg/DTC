fe5e3fc008c36a863b2c787da3e450a36b93b9f1
Arnold Agus <Arnold.Agus@dataprocessors.com.au>
2016-07-15 14:40:52 +1000

    DTC-2 first iteration of recursive data representation

--- components/branch.js
+++ components/branch.js
@@ -1,3 +1,25 @@
 import React from 'react'
 import { Component } from 'react'
 import { connect } from 'react-redux'
+
+export class Branch extends Component {
+
+	constructor(props) {
+		super(props)
+		console.log("here are our props from the branch");
+		console.log(props)
+		// console.log(this.props.data)
+	}
+
+	render() {
+		return (
+
+			<div>
+				We have recieved a branch here!
+			</div>
+			)
+
+	}
+}
+
+export default Branch
--- components/tree.js
+++ components/tree.js
@@ -1,34 +1,122 @@
 import React from 'react'
 import { Component } from 'react'
 import { connect } from 'react-redux'
-import branch from './branch';
+import { Branch } from './branch';
 
+console.log(Branch)
 export class Tree extends Component {
 	constructor(props) {
 		super(props)
-		console.log("here are our props")
-		console.log(props)
+		this.renderChild = this.renderChild.bind(this)
+		this.dictionaryRender = this.dictionaryRender.bind(this)
+		console.log(this.dictionaryRender)
+	}
+
+	renderChild(childTree) {
+		return (
+			<Branch data={childTree}/>
+			)
+	}
+
+	renderLeaf(leaf) {
+		console.log("we are rendering our leaf")
+		return (
+			<div >
+				I am a leaf node {leaf}
+			</div>
+			)
+	}
+
+	dictionaryRender(value, key) {
+		console.log("inside dictionary Render");
+		console.log(value, key)
+	}
+
+	typeChecker(value, index) {
+		console.log("going through the type checker")
+		console.log(value)
+		switch (typeof value) {
+			case 'object':
+				let keyChar = 'a';
+				return Object.keys(value).map((attribute)=> {
+					let key = index.toString() + keyChar;
+					keyChar = String.fromCharCode(keyChar.charCodeAt() + 1);
+					switch (typeof value[attribute]) {
+						case 'string':
+							console.log("stringified");
+							return (
+								<p key={key}>value here: {value[attribute]} </p>
+							)
+							break
+						case 'object':
+							console.log("objectfy")
+							return (
+								<Branch key={key} data={value[attribute]}/>
+							)
+							break
+						case 'array':
+							console.log("arrying")
+							break
+						default:
+							console.log("everything else")
+					}
+
+				})
+			default:
+				console.log("probably at a leaf node");
+		}
 	}
 
 	render() {
 
-		Object.keys(this.props).forEach((attribute)=> {
-			if (typeof this.props[attribute] == 'object') {
-				console.log("inside here we are recursing through")
-				console.log(this.props[attribute])
-				return (
-					<Tree data={this.props.attribute}/>
-					)
-			} else {
-				console.log("We found a leaf node");
-				return (
-					<div>
-						hello we have our node
-						{this.props[attribute]}
-					</div>
-					)
+		let branches = <Branch data={this.props.level1a}/>
+
+		let values = [];
+
+		let rendering = Object.keys(this.props).forEach((attribute)=> {
+			if (typeof this.props[attribute] != 'function') {
+				values.push(this.props[attribute])
 			}
-		})
+		});
+
+		console.log("here is our values")
+
+		console.log(values)
+
+		let renderedValues = values.map(this.typeChecker)
+
+		console.log("here are our rendered values");
+
+		console.log(renderedValues)
+		return (
+			<div>
+				{renderedValues}
+				<br/>
+				<hr/>
+				<Branch data={this.props.level1}/>
+			</div>
+			)
+
+
+
+		// Object.keys(this.props).forEach((attribute)=> {
+		// 	if (typeof this.props[attribute] == 'object') {
+		// 		console.log("inside here we are recursing through")
+		// 		console.log(this.props[attribute])
+		// 		return (
+		// 			<div>
+		// 				<Branch data={this.props[attribute]}/>
+		// 			</div>
+		// 			)
+		// 	} else {
+		// 		console.log("leaf part")
+		// 		return (
+		// 			<div>
+		// 				hello we have our node
+		// 			</div>
+		// 			)
+		// 	}
+		// })
 	}
 
 }
