ba5c8ca7442e0427b7e258cdbfd412cd3b59fc56
Arnold Agus <Arnold.Agus@dataprocessors.com.au>
2016-07-18 16:41:10 +1000

    Adjusted webpack config failure

--- webpack.config.js
+++ webpack.config.js
@@ -18,29 +18,15 @@ module.exports = {
   ],
   module: {
     loaders: [
-
       {
         test: /\.js$/,
         loaders: [ 'babel' ],
         exclude: /node_modules/,
         include: __dirname
       },
-      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
-
-      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
-      // loads bootstrap's css.
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" },
-      {
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
-      },
-
       {
         test: /\.css$/,
-        loaders: 'style-loader!css-loader'
+        loader: "css-loader"
       }
     ]
   },
